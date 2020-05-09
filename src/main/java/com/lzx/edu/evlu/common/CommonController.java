6371aab021f886917fc2519cda90d793140f8f7d
18801021782 <756287014@qq.com>
2020-05-09 17:41:34 +0800

    第四次提交

--- src/main/java/com/lzx/edu/evlu/common/CommonController.java
+++ src/main/java/com/lzx/edu/evlu/common/CommonController.java
@@ -6,6 +6,9 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class CommonController {
 
@@ -16,7 +19,14 @@ public class CommonController {
 
     @RequestMapping("/stock")
     public String getStock() {
-        synchronized (this){
+
+        String projectKey = "loadkey";
+        String clentId = UUID.randomUUID().toString();
+        try {
+            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(projectKey, clentId, 10, TimeUnit.SECONDS);//jedis.setnx()
+            if(!result){
+                return "error";
+            }
             Integer stock = Integer.valueOf(stringRedisTemplate.opsForValue().get("stock"));
             if (stock > 0) {
                 int realStock = stock - 1;
@@ -25,7 +35,12 @@ public class CommonController {
             } else {
                 System.out.println("扣减失败，库存不足");
             }
+        }finally {
+            if(stringRedisTemplate.opsForValue().get(projectKey).equalsIgnoreCase(clentId)){
+                stringRedisTemplate.delete(projectKey);
+            }
         }
+
         return "end";
     }
 
